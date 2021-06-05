38ab7cc7bd7b72bb3c1f72db9475e691b5e9efb3
vl4deee11 <boi4enkovlad@yandex>
2021-06-05 14:59:53 +0100

    added ctx

--- ch/httpdriver.go
+++ ch/httpdriver.go
@@ -2,19 +2,27 @@ package ch
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 type HTTPD struct {
 	url string
+	cli *http.Client
+	defaultCtx context.Context
 }
 
 func (d *HTTPD) SetHTTP(h string, p int) {
 	// TODO: add tls and auth
+	d.cli = &http.Client{
+		Timeout: 60 * time.Second,
+	}
 	d.url = d.httpF(h, p)
+
 }
 
 func (d *HTTPD) httpF(h string, p int) string {
@@ -22,7 +30,13 @@ func (d *HTTPD) httpF(h string, p int) string {
 }
 
 func (d *HTTPD) Get(q string) ([]map[string]interface{}, error) {
-	resp, err := http.Post(d.url, "application/json", bytes.NewReader([]byte(q)))
+	req, err := http.NewRequestWithContext(context.Background(), "POST", d.url, bytes.NewBuffer([]byte(q)))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := d.cli.Do(req)
 	if err != nil {
 		return nil, err
 	}
